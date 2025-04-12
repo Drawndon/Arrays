f6f52cf07434ca8848f7f0f6f3324c8ce3ce80ad
Андрей Полищук <dcnfandrey@mail.ru>
2025-04-12 16:44:56 +0300

    Wrote code for "Shift"

--- Shift/main.cpp
+++ Shift/main.cpp
@@ -4,32 +4,22 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "");
-	const int n = 5;
-	int arr[n];
-	int s = 0;
-	double average = 0;
-	int min_el = INT_MAX, max_el = INT_MIN;
-	cout << "Введите пять элементов массива:" << endl;
-	for (int i = 0; i < n; i++)
+	const int n = 10;
+	int arr[n] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	cout << "Исходный массив:" << endl;
+	for (int i = 0; i < n; i++) cout << arr[i] << " ";
+	int shift;
+	cout << endl << "Введите число, на которое вы хотите сдвинуть элементы массива влево: "; cin >> shift;
+	int t;
+	for (int i = 0; i < shift % n; i++)
 	{
-		cin >> arr[i];
-		if (min_el > arr[i])
-			min_el = arr[i];
-		if (arr[i] > max_el)
-			max_el = arr[i];
-		s += arr[i];
+		t = arr[0];
+		for (int j = 1; j < n; j++)
+		{
+			arr[j - 1] = arr[j];
+		}
+		arr[n - 1] = t;
 	}
-	average = (double)s / n;
-	cout << endl << "1. Вывод в прямом порядке:" << endl;
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << "\t";
-	}
-	cout << endl << "2. Вывод в обратном порядке:" << endl;
-	for (int i = n - 1; i > -1; i--)
-	{
-		cout << arr[i] << "\t";
-	}
-	cout << endl << "3. Сумма: " << s << endl << "4. Среднее арифметическое: " << average << endl;
-	cout << "5. Минимальный элемент: " << min_el << endl << "6. Максимальный элемент: " << max_el << endl;
+	cout << endl << "Сдвинутый массив:" << endl;
+	for (int i = 0; i < n; i++) cout << arr[i] << " ";
 }
