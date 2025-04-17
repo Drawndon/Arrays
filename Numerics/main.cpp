5a5346a67330c357a46565af15bb9faa1ceaf6c9
Андрей Полищук <dcnfandrey@mail.ru>
2025-04-17 21:36:40 +0300

    Numerics corrected

--- Numerics/main.cpp
+++ Numerics/main.cpp
@@ -1,6 +1,6 @@
 ﻿#include <iostream>
 using namespace std;
-//#define BINARY_CONVERT
+#define BINARY_CONVERT
 #define HEX_CONVERT
 void main()
 {
@@ -9,6 +9,7 @@ void main()
 	const int n = 16;
 	int arr[n];
 	int number, i;
+	cout << "Программа перевода десятичного числа в двоичное" << endl;
 	cout << "Введите десятичное число: "; cin >> number;
 	int temp = number;
 	for (i = 0; temp > 0; i++)
@@ -25,22 +26,27 @@ void main()
 	
 #ifdef HEX_CONVERT
 	const int size = 100;
-	char hex_arr[size];
-	int number_1, i = 1, remainder;
+	int hex_arr[size];
+	int number_1, remainder;
+	i = 0;
+	cout << endl << "Программа перевода десятичного числа в шестнадцатиричное" << endl;
 	cout << "Введите десятичное число: "; cin >> number_1;
 	int temp_1 = number_1;
 	while (temp_1 != 0)
 	{
 		remainder = temp_1 % 16;
-		if (remainder < 10) remainder += 48;
-		else remainder += 55;
 		hex_arr[i++] = remainder;
 		temp_1 /= 16;
 
 	}
 	cout << "Число " << number_1 << " в шестнадцатиричной системе равно: ";
-	for (int j = i - 1; j > 0; j--)
-		cout << hex_arr[j];
+	cout << "0x";
+	for (int j = i - 1; j >= 0; j--)
+	{
+		if (hex_arr[j] < 10)
+			cout << hex_arr[j];
+		else cout << char(hex_arr[j] + 55);
+	}
 	cout << endl;
 #endif // HEX_CONVERT
 
