02a7e7551a705eb7db7bf173bc514a24a86feb67
Андрей Полищук <dcnfandrey@mail.ru>
2025-04-17 05:46:18 +0300

    Changed encoding

--- Numerics/main.cpp
+++ Numerics/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+ï»¿#include <iostream>
 using namespace std;
 //#define BINARY_CONVERT
 #define HEX_CONVERT
@@ -9,14 +9,14 @@ void main()
 	const int n = 16;
 	int arr[n];
 	int number, i;
-	cout << "Ââåäèòå äåñÿòè÷íîå ÷èñëî: "; cin >> number;
+	cout << "Ð’Ð²ÐµÐ´Ð¸Ñ‚Ðµ Ð´ÐµÑÑÑ‚Ð¸Ñ‡Ð½Ð¾Ðµ Ñ‡Ð¸ÑÐ»Ð¾: "; cin >> number;
 	int temp = number;
 	for (i = 0; temp > 0; i++)
 	{
 		arr[i] = temp % 2;
 		temp /= 2;
 	}
-	cout << "×èñëî " << number << " â äâîè÷íîé ñèñòåìå ðàâíî: ";
+	cout << "Ð§Ð¸ÑÐ»Ð¾ " << number << " Ð² Ð´Ð²Ð¾Ð¸Ñ‡Ð½Ð¾Ð¹ ÑÐ¸ÑÑ‚ÐµÐ¼Ðµ Ñ€Ð°Ð²Ð½Ð¾: ";
 	for (i = i - 1; i >= 0; i--)
 		cout << arr[i];
 	cout << endl;
@@ -27,7 +27,7 @@ void main()
 	const int size = 100;
 	char hex_arr[size];
 	int number_1, i = 1, remainder;
-	cout << "Ââåäèòå äåñÿòè÷íîå ÷èñëî: "; cin >> number_1;
+	cout << "Ð’Ð²ÐµÐ´Ð¸Ñ‚Ðµ Ð´ÐµÑÑÑ‚Ð¸Ñ‡Ð½Ð¾Ðµ Ñ‡Ð¸ÑÐ»Ð¾: "; cin >> number_1;
 	int temp_1 = number_1;
 	while (temp_1 != 0)
 	{
@@ -38,7 +38,7 @@ void main()
 		temp_1 /= 16;
 
 	}
-	cout << "×èñëî " << number_1 << " â øåñòíàäöàòèðè÷íîé ñèñòåìå ðàâíî: ";
+	cout << "Ð§Ð¸ÑÐ»Ð¾ " << number_1 << " Ð² ÑˆÐµÑÑ‚Ð½Ð°Ð´Ñ†Ð°Ñ‚Ð¸Ñ€Ð¸Ñ‡Ð½Ð¾Ð¹ ÑÐ¸ÑÑ‚ÐµÐ¼Ðµ Ñ€Ð°Ð²Ð½Ð¾: ";
 	for (int j = i - 1; j > 0; j--)
 		cout << hex_arr[j];
 	cout << endl;
