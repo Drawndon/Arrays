1b066c7cda91b8454134bfa9392323593fb16a90
Андрей Полищук <dcnfandrey@mail.ru>
2025-05-05 21:49:16 +0300

    Determinant DONE

--- Determinant/main.cpp
+++ Determinant/main.cpp
@@ -19,4 +19,14 @@ void main()
 			cout << arr[i][j] << tab;
 		cout << endl;
 	}
+	int p1, p2, p3, p4, p5, p6;
+	p1 = arr[0][0] * arr[1][1] * arr[2][2];
+	p2 = arr[1][0] * arr[2][1] * arr[0][2];
+	p3 = arr[0][1] * arr[1][2] * arr[2][0];
+	p4 = arr[0][2] * arr[1][1] * arr[2][0];
+	p5 = arr[0][1] * arr[1][0] * arr[2][2];
+	p6 = arr[2][1] * arr[1][2] * arr[0][0];
+	int s1 = p1 + p2 + p3;
+	int s2 = p4 + p5 + p6;
+	cout << "Determinant: " << s1 - s2 << endl;
 }
